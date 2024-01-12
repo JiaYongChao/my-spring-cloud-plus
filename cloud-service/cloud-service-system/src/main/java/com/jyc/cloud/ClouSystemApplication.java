15c64f82a16b6fa1a0310eac4c262f76f496bccd
JiaYongChao <jiayongchao258@163.com>
2024-01-12 19:39:37 +0800

    初始化项目，集成springcloud

---
+++ cloud-service/cloud-service-system/src/main/java/com/jyc/cloud/ClouSystemApplication.java
@@ -0,0 +1,19 @@
+package com.jyc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassNAME ClouSystemApplication
+ * @Description 系统业务启动类
+ * @Author jiayongchao
+ * @Date 2024/1/12 19:28
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ClouSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClouSystemApplication.class,args);
+    }
+
+}
